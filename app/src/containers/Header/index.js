07e1d27d09fc352ff2e8ea44c69e7084169631bd
Richard Griffiths <cwolfs@gmail.com>
2018-06-11 15:15:24 +0200

    Add real menu component in the header and disable some animations

--- app/src/containers/Header/index.js
+++ app/src/containers/Header/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Menu } from 'antd';
 
 import './Header.css';
 
+const MenuItem = Menu.Item;
+const { SubMenu } = Menu;
+
 export default () => (
-  <div className="header">File  Edit  Configure</div>
+  <div className="header">
+    <Menu mode="horizontal">
+      <SubMenu title="File">
+        <MenuItem>Open Folder</MenuItem>
+        <MenuItem>Save Conversation</MenuItem>
+        <MenuItem>Save Conversation As...</MenuItem>
+        <MenuItem>Export as JSON</MenuItem>
+      </SubMenu>
+      <SubMenu title="Options">
+        <MenuItem>Option 1</MenuItem>
+      </SubMenu>
+    </Menu>
+  </div>
 );
