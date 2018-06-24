4047ab18305eeafc7d928b845c16b1699e4f0b36
Richard Griffiths <cwolfs@gmail.com>
2018-06-24 08:53:17 +0200

    Added branch and root deleting. Unselect active node if deleted.

--- app/src/stores/nodeStore/index.js
+++ app/src/stores/nodeStore/index.js
@@ -99,28 +99,63 @@ class NodeStore {
     const { unsavedActiveConversationAsset } = dataStore;
     const { branches } = node;
 
-    branches.forEach((branch) => {
-      const { auxiliaryLink } = branch;
-      const id = getId(branch.idRef);
+    if (node.type === 'node') {
+      branches.forEach(branch => this.deleteBranchCascade(branch));
 
-      if (!auxiliaryLink) {
-        const nextNode = this.nodes.get(branch.nextNodeIndex);
-        if (nextNode) this.deleteNodeCascade(nextNode);
-      }
+      remove(
+        unsavedActiveConversationAsset.Conversation.nodes,
+        (convNode) => {
+          const toDelete = (getId(convNode.idRef) === getId(node.idRef));
+          return toDelete;
+        },
+      );
 
-      this.branches.delete(id);
-    });
+      if (this.activeNode && (getId(this.activeNode) === getId(node))) this.unselectActiveNode();
+      this.nodes.delete(node.index);
+    } else if (node.type === 'response') {
+      this.deleteBranchCascade(node);
+    } else if (node.type === 'root') {
+      this.deleteBranchCascade(node);
 
-    remove(
-      unsavedActiveConversationAsset.Conversation.nodes,
-      (convNode) => {
-        const toDelete = (getId(convNode.idRef) === getId(node.idRef));
-        return toDelete;
-      },
-    );
+      remove(
+        unsavedActiveConversationAsset.Conversation.roots,
+        (convRoot) => {
+          const toDelete = (getId(convRoot.idRef) === getId(node.idRef));
+          return toDelete;
+        },
+      );
 
-    this.nodes.delete(node.index);
-    // this.setRebuild(true);
+      if (this.activeNode && (getId(this.activeNode) === getId(node))) this.unselectActiveNode();
+      this.roots.delete(getId(node));
+    }
+  }
+
+  @action unselectActiveNode() {
+    this.activeNode = null;
+  }
+
+  @action deleteBranchCascade(branch) {
+    // const { unsavedActiveConversationAsset } = dataStore;
+    const { auxiliaryLink, parentId } = branch;
+    const id = getId(branch.idRef);
+
+    if (!auxiliaryLink) {
+      const nextNode = this.nodes.get(branch.nextNodeIndex);
+      if (nextNode) this.deleteNodeCascade(nextNode);
+    }
+
+    if (this.activeNode && (getId(this.activeNode) === getId(branch))) this.unselectActiveNode();
+    this.branches.delete(id);
+    const parentNode = this.nodes.get(parentId);
+    if (parentNode) {
+      remove(
+        parentNode.branches,
+        (nodeBranch) => {
+          const toDelete = (getId(nodeBranch.idRef) === getId(branch.idRef));
+          return toDelete;
+        },
+      );
+    }
   }
 
   buildRoots(roots) {
@@ -139,11 +174,12 @@ class NodeStore {
     });
   }
 
-  buildBranches(branches) {
+  buildBranches(parent, branches) {
     branches.forEach((branch) => {
       const id = getId(branch.idRef);
       this.branches.set(id, branch);
       branch.type = 'response';
+      branch.parentId = parent.index;
     });
   }
 
@@ -173,7 +209,7 @@ class NodeStore {
     }
 
     const childNode = this.nodes.get(nextNodeIndex);
-    this.buildBranches(childNode.branches);
+    this.buildBranches(childNode, childNode.branches);
 
     return [
       {
