2b97b8e2afeeb501eced2acf7fb19aa479ae2019
Richard Griffiths <cwolfs@gmail.com>
2018-06-25 08:26:57 +0200

    Removed intl for mobx upgrade prep

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Provider, observer } from 'mobx-react';
-import { MobxIntlProvider } from 'mobx-react-intl';
 
 import stores from './stores';
 
@@ -14,11 +13,9 @@ export default class App extends Component {
   render() {
     return (
       <Provider {...stores}>
-        <MobxIntlProvider>
-          <Router>
-            <Route component={Main} />
-          </Router>
-        </MobxIntlProvider>
+        <Router>
+          <Route component={Main} />
+        </Router>
       </Provider>
     );
   }
