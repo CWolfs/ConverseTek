87e6c2116e6680963cac085f91e0b93165efcbdd
Richard Griffiths <cwolfs@gmail.com>
2018-07-04 00:06:18 +0200

    Some exp work around conditions UX

--- app/src/containers/ConversationConditions/index.js
+++ app/src/containers/ConversationConditions/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observer, inject } from 'mobx-react';
-import { Button, Icon, List } from 'antd';
+import { Button, Icon, List, Select } from 'antd';
 import classnames from 'classnames';
+
+const { Option } = Select;
 
 @observer
 class ConversationConditions extends Component {
@@ -10,14 +12,26 @@ class ConversationConditions extends Component {
     super(props);
 
     this.renderListItem = this.renderListItem.bind(this);
-    this.onItemRemoved = this.onItemRemoved(this);
+    this.onItemRemoved = this.onItemRemoved.bind(this);
+    this.onSearch = this.onSearch.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onItemRemoved(index) {
 
   }
 
-  renderListItem(item, index) {
+  onSearch(condition, value) {
+    condition.functionName = value;
+  }
+
+  onChange(condition, option) {
+    condition.functionName = option.key;
+  }
+
+  renderListItem(condition, index) {
+    const { defStore } = this.props;
+    const { operations } = defStore;
     const key = index;
 
     const classes = classnames({
@@ -35,7 +49,25 @@ class ConversationConditions extends Component {
           </Button>,
         ]}
       >
-        {item.functionName}
+        <Select
+          mode="combobox"
+          showSearch
+          onSearch={value => this.onSearch(condition, value)}
+          onChange={value => this.onChange(condition, value)}
+          value={{ key: condition.functionName, label: 'test' }}
+          style={{ width: 250 }}
+          labelInValue
+          optionLabelProp="title"
+        >
+          {operations.map(operation => (
+            <Option
+              key={operation.Key}
+              title={operation.Label}
+            >
+              {operation.Label}
+            </Option>
+          ))}
+        </Select>
       </List.Item>
     );
   }
@@ -62,8 +94,8 @@ class ConversationConditions extends Component {
 
 ConversationConditions.propTypes = {
   dataStore: PropTypes.object.isRequired,
-  nodeStore: PropTypes.object.isRequired,
+  defStore: PropTypes.object.isRequired,
   node: PropTypes.object.isRequired,
 };
 
-export default inject('dataStore', 'nodeStore')(ConversationConditions);
+export default inject('dataStore', 'defStore')(ConversationConditions);
