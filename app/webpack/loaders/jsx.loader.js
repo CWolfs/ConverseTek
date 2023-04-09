b6d1caeaa83a0dc4a91e068b8fa088c03e6736dc
Richard Griffiths <cwolfs@gmail.com>
2023-04-09 22:58:19 +0200

    Added TS

--- app/webpack/loaders/jsx.loader.js
+++ app/webpack/loaders/jsx.loader.js
@@ -4,8 +4,14 @@ var webpack = require('webpack');
 module.exports = function (config, APP_DIR) {
   console.log('Using jsx loader');
 
+  // config.module.rules.push({
+  //   test: /\.jsx?$/,
+  //   use: ['babel-loader?cacheDirectory'],
+  //   include: APP_DIR,
+  // });
+
   config.module.rules.push({
-    test: /\.jsx?$/,
+    test: /\.(js|jsx|tsx|ts)$/,
     use: ['babel-loader?cacheDirectory'],
     include: APP_DIR,
   });
