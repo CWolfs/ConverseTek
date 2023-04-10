760eb2e295a7cbad81ed12d2bfbe71dab7c524d1
Richard Griffiths <cwolfs@gmail.com>
2023-04-10 19:10:11 +0200

    Cleaned up old eslint config

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,30 +1,3 @@
-// {
-//   "extends": ["airbnb"],
-//   "rules": {
-//     "linebreak-style": 0,
-//     "react/self-closing-comp": 0,
-//     "react/jsx-filename-extension": 0,
-//     "react/forbid-prop-types": 0,
-//     "jsx-a11y/anchor-is-valid": [
-//       "error",
-//       {
-//         "components": ["Link"],
-//         "specialLink": ["to"]
-//       }
-//     ],
-//     "max-len": ["error", { "code": 140 }],
-//     "arrow-parens": ["error", "always"],
-//     "object-curly-newline": 0,
-//     "function-paren-newline": ["error", "multiline"]
-//   },
-//   "parser": "babel-eslint",
-//   "globals": {
-//     "window": true,
-//     "__BUILD_DATE__": true,
-//     "boundControllerAsync": true
-//   }
-// }
-
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
   parser: '@typescript-eslint/parser',
@@ -35,4 +8,9 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   root: true,
+  globals: {
+    window: true,
+    __BUILD_DATE__: true,
+    boundControllerAsync: true,
+  },
 };
