4d564fdc1e89e09ea97679d12501b41a896af730
Richard Griffiths <cwolfs@gmail.com>
2023-04-10 19:30:43 +0200

    Upgrade react-router-dom to lastest 4.x version, tsconfig tweak

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Provider, observer } from 'mobx-react';
 
@@ -8,16 +8,12 @@ import Main from './layouts/Main';
 
 import './css/styles.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Provider {...stores}>
-        <Router>
-          <Route component={Main} />
-        </Router>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider {...stores}>
+    <Router>
+      <Route component={Main} />
+    </Router>
+  </Provider>
+);
 
 export default observer(App);
